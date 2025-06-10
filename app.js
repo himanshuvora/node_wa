d238b48b7efb1b3e3d59d1cdc23919e5096479fc
himanshu-laptop <himanshu.vora@gmail.com>
2025-06-10 11:17:31 +0530

    remove https

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 const http = require('http');
 
 const options = {
